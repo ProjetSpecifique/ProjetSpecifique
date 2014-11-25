aa746ff9f5f8cc69b7fa401657b868dff9a397ab
somarariana <somarariana@yahoo.com>
2014-11-25 10:49:15 +0100

    structure the java project

---
+++ LireDescriptorsComputation/src/descripteurs/EdgeImpl.java
@@ -0,0 +1,11 @@
+package descripteurs;
+
+public class EdgeImpl extends MyDescriptor {
+
+	@Override
+	public byte[] computeHistogram() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
