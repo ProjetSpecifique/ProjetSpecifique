d5b48e77580f02020aa5109fc7134f5fbdaa1d84
somarariana <somarariana@yahoo.com>
2014-11-25 21:02:06 +0100

    Implement descriptors [part 1]

--- LireDescriptorsComputation/src/descripteurs/MyDescriptor.java
+++ LireDescriptorsComputation/src/descripteurs/MyDescriptor.java
@@ -1,5 +1,43 @@
 package descripteurs;
 
+import java.awt.image.BufferedImage;
+
 public abstract class MyDescriptor {
-	public abstract byte[] computeHistogram();
+
+	private BufferedImage image;
+	protected Object lireDescriptor;
+
+	/*
+	 * All LIRE descriptors implement LireFeature interface which offers defines
+	 * the extract method
+	 */
+	public MyDescriptor(BufferedImage img) {
+		this.image = img;
+	}
+
+	/**
+	 * Computes the descriptor's histogram and returns it as an array of double
+	 * 
+	 * PS: All LIRE descriptors implement LireFeature interface which offers 2
+	 * getters for the result
+	 * 
+	 * 1. getByteArrayRepresentation() : returns an array of bytes, but
+	 * sometimes stuffs 2 numbers into one byte
+	 * 
+	 * 2.getDoubleHistogram() : stupid name, there are not 2 histograms but a
+	 * histogram of doubles; some descriptors return integer values and they are
+	 * converted into double using ConversionUtils
+	 * */
+	public abstract double[] computeHistogram();
+
+	/* getter image */
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	/* setter image */
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
 }
